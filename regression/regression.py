66458375a083af59fb932b9a685bcf0a204f2a8d
TreyAmador <tmamador@cpp.edu>
2018-04-06 03:46:06 -0700

    plot target vs prediction

--- regression/regression.py
+++ regression/regression.py
@@ -7,47 +7,6 @@ import matplotlib.pyplot as plt
 from copy import deepcopy
 from scipy import linalg
 
-from random import randint
-from random import Random
-
-from time import time
-
-
-
-
-def lin_reg_ex():
-    import matplotlib.pyplot as plt
-    import numpy as np
-    from sklearn import datasets, linear_model
-    from sklearn.metrics import mean_squared_error, r2_score
-
-    #diabetes = datasets.load_diabetes()
-    
-    # for testing
-    diabetes = datasets.load_boston()
-
-
-    diabetes_X = diabetes.data[:, np.newaxis, 2]
-    diabetes_X_train = diabetes_X[:-20]
-    diabetes_X_test = diabetes_X[-20:]
-    diabetes_y_train = diabetes.target[:-20]
-    diabetes_y_test = diabetes.target[-20:]
-
-    regr = linear_model.LinearRegression()
-    regr.fit(diabetes_X_train, diabetes_y_train)
-    diabetes_y_pred = regr.predict(diabetes_X_test)
-
-    print('Coefficients: \n', regr.coef_)
-    print("Mean squared error: %.2f"
-          % mean_squared_error(diabetes_y_test, diabetes_y_pred))
-    print('Variance score: %.2f' % r2_score(diabetes_y_test, diabetes_y_pred))
-
-    plt.scatter(diabetes_X_test, diabetes_y_test,  color='black')
-    plt.plot(diabetes_X_test, diabetes_y_pred, color='blue', linewidth=3)
-    plt.xticks(())
-    plt.yticks(())
-    plt.show()
-
     
 def clone(*args):
     return tuple(deepcopy(x) for x in args)
@@ -71,30 +30,27 @@ def insert_weight(data):
     return np.insert(data,0,[1 for x in range(data.shape[0])],axis=1)
 
 
-def plotter(x_test,y_test,y_pred):
-    y_min,y_max = np.amin(y_test),np.amax(y_test)
-    fig,ax = plt.subplots()
-    ax.scatter(y_test,y_pred,edgecolors=(0,0,0))
-    ax.plot([y_min,y_max],[y_min,y_max],'k--',lw=4)
+def plot_one(ax,target,predict):
+    ax.scatter(target,predict,edgecolors=(0,0,0))
+    data_min,data_max = np.amin(target),np.amax(target)
+    ax.plot([data_min,data_max],[data_min,data_max],'k--',lw=4)
     ax.set_xlabel('Measured')
     ax.set_ylabel('Predicted')
+
+
+def plot(target,*predictions):
+    fig, ax = plt.subplots(len(predictions), sharex=True)
+    for index,predict in enumerate(predictions):
+        plot_one(ax[index],target,predict)
     plt.show()
-
-
-def plot(x_test,y_test,y_pred):
-    plt.scatter(x_test,y_test,color='blue',edgecolor='black')
-    plt.plot(x_test,y_pred,color='red',linewidth=3)
-    plt.xticks(())
-    plt.yticks(())
-    plt.show()
-
+    
 
 def linear_regression_scikit(train_data,test_data,train_target,test_target):
     regr = linear_model.LinearRegression()
     regr.fit(train_data,train_target)
-    target_predict = regr.predict(test_data)
-    mse = mean_squared_error(test_target,target_predict)
-    return target_predict,mse
+    predict = regr.predict(test_data)
+    mse = mean_squared_error(test_target,predict)
+    return regr.coef_,predict,mse
 
 
 def calculate_decomposition(t,X,w):
@@ -110,11 +66,11 @@ def linear_regression_analytical(train_data,test_data,train_target,test_target):
     train_data_w = insert_weight(train_data)
     test_data_w = insert_weight(test_data)
     inver = linalg.inv(np.matmul(train_data_w.T,train_data_w))
-    mtx_tar = np.matmul(inver,train_data_w.T)
-    weights = np.matmul(mtx_tar,train_target)
+    weights = np.matmul(np.matmul(inver,train_data_w.T),train_target)
+    predict = np.matmul(test_data_w,weights)
     decomp = calculate_decomposition(test_target,test_data_w,weights)
     mse = calculate_mean_squared_error(test_data.shape[0],decomp.T,decomp)
-    return weights,mse[0][0]
+    return weights.T,predict,mse[0][0]
 
 
 def calculate_gradient(data,target,weights):
@@ -123,34 +79,30 @@ def calculate_gradient(data,target,weights):
 
 
 def linear_regression_numerical(train_data,test_data,train_target,test_target):
-
     train_data_w = insert_weight(train_data)
     test_data_w = insert_weight(test_data)
-
-    weights = np.full((train_data_w.shape[1],1),0.0)
-    decomp = calculate_decomposition(test_target,test_data_w,weights)
-    mse = calculate_mean_squared_error(test_data_w.shape[0],decomp.T,decomp)
+    weights = np.full((train_data_w.shape[1],1),3.0)
     epsilon = 0.000006476
-    rounds = 1000000
-
+    rounds = 500000
     for _ in range(rounds):
         gradient = calculate_gradient(train_data_w,train_target,weights)
         weights += gradient * epsilon
-
+    predict = np.matmul(test_data_w,weights)
     decomp = calculate_decomposition(test_target,test_data_w,weights)
     mse = calculate_mean_squared_error(test_data.shape[0],decomp.T,decomp)
-    println('target data:',mse)
+    return weights.T,predict,mse[0][0]
 
 
 def driver():
     train_data,test_data,train_target,test_target = split_data()
-    prediction,mse = linear_regression_scikit(train_data,test_data,train_target,test_target)
-    weights,l_e = linear_regression_analytical(train_data,test_data,train_target,test_target)
-    
-    
-    linear_regression_numerical(train_data,test_data,train_target,test_target)
-
-    
+    weight_sk,predict_sk,mse_sk = linear_regression_scikit(train_data,test_data,train_target,test_target)
+    weight_a,predict_a,mse_a = linear_regression_analytical(train_data,test_data,train_target,test_target)
+    weight_n,predict_n,mse_n = linear_regression_numerical(train_data,test_data,train_target,test_target)
+    print(
+        'sklearn linear regression\n',weight_sk,mse_sk,'\n\n',
+        'Analyitical Linear Regression\n',weight_a,mse_a,'\n\n',
+        'Numerical Linear Regression',weight_n,mse_n,'\n\n')
+    plot(test_target,predict_sk,predict_a,predict_n)
 
 
 if __name__ == '__main__':
