1df3a91a95fadb996733acdf33aecfb28cce251a
Trey Amador <tmamador@cpp.edu>
2018-04-28 10:08:42 -0700

    dimension tighter-fitting

--- neural/preprocessing.py
+++ neural/preprocessing.py
@@ -8,6 +8,18 @@ import cv2
 import os
 
 
+# DEFAULT_DIMENSION = 500
+# BORDER_NORM = 3
+
+
+# DEFAULT_DIMENSION = 384
+BORDER_NORM = 4
+
+
+# WIDTH_BORDER = 5
+# HEIGHT_BORDER = 3
+
+
 detector = dlib.get_frontal_face_detector()
 win = dlib.image_window()
 
@@ -44,8 +56,12 @@ def process_img(img):
 
     det = det[0]
     btm, top, rgt, lft = det.bottom(), det.top(), det.right(), det.left()
-    wth, hgt = (rgt-lft)//3, (btm-top)//3
+    wth, hgt = (rgt-lft)//BORDER_NORM, (btm-top)//BORDER_NORM
     top_off, btm_off, lft_off, rgt_off = top-hgt, btm+hgt, lft-wth, rgt+wth
+
+    # shifting offset up
+    top_off -= hgt
+    # btm_off -= hgt
 
     top_off = top_off if top_off > 0 else 0
     btm_off = btm_off if btm_off < img.shape[0] else img.shape[0]
@@ -53,30 +69,36 @@ def process_img(img):
     rgt_off = rgt_off if rgt_off < img.shape[1] else img.shape[1]
 
     img = img[top_off:btm_off, lft_off:rgt_off, ]
-    img = transform.resize(img, (500, 500,))
+
+    # TODO remove this, or write it programmatically
+    # img = transform.resize(img, (DEFAULT_DIMENSION, DEFAULT_DIMENSION,))
 
     if len(img.shape) == 2:
         img = color.gray2rgb(img)
     return img
 
 
-def import_imgs():
+def run():
     bp = 'data/train'
-    mp = 'data/modtrain'
+    mp = 'data/modtrain'+'-d'+str('STD')+'-crop-b'+str(BORDER_NORM)
+    os.mkdir(mp)
     exts = [x for x in os.listdir(bp)]
     paths = [bp+'/'+x for x in exts]
     directory = read_training()
     imgs = io.imread_collection(paths)
-    with open('data/modtrain_target.csv', 'wt') as f_obj:
+    ttl = len(exts)
+    with open(mp+'_target.csv', 'wt') as f_obj:
+        itr = 1
         for ext, img in zip(exts, imgs):
             img = process_img(img)
             if img is not None:
                 f_obj.write(ext+','+str(directory[ext])+'\n')
                 io.imsave(mp+'/'+ext, img)
+            print(itr, 'of', ttl)
+            itr += 1
 
-
-def run():
-    import_imgs()
+            if itr > 100:
+                break
 
 
 if __name__ == '__main__':
